7967b13e49089ac09a231769e3981945d7cfdcc5
Elias Turrin <turrinelias@gmail.com>
2026-02-20 16:49:29 -0300

    fix: remove invalid ON CONFLICT from auth.identities in seed.sql

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -36,7 +36,7 @@ VALUES (
 INSERT INTO auth.identities (id, user_id, identity_data, provider, last_sign_in_at, created_at, updated_at)
 VALUES (
     '11111111-1111-1111-1111-111111111111', '11111111-1111-1111-1111-111111111111', '{"sub":"11111111-1111-1111-1111-111111111111","email":"admin@example.com"}', 'email', now(), now(), now()
-) ON CONFLICT (provider, id) DO NOTHING;
+);
 
 
 -- customer@example.com
@@ -49,7 +49,7 @@ VALUES (
 INSERT INTO auth.identities (id, user_id, identity_data, provider, last_sign_in_at, created_at, updated_at)
 VALUES (
     '22222222-2222-2222-2222-222222222222', '22222222-2222-2222-2222-222222222222', '{"sub":"22222222-2222-2222-2222-222222222222","email":"customer@example.com"}', 'email', now(), now(), now()
-) ON CONFLICT (provider, id) DO NOTHING;
+);
 
 
 -- ============================================
